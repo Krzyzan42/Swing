0877de0dabc32ae1c291aea827cc573cc6adeafc
lmProgramming <mikikubs@gmail.com>
2025-05-26 13:52:00 +0200

    fix: setting texts in settings from player prefs now

--- Assets/MainMenu/SettingsManager.cs
+++ Assets/MainMenu/SettingsManager.cs
@@ -1,4 +1,7 @@
+using System.Globalization;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MainMenu
 {
@@ -6,18 +9,34 @@ namespace MainMenu
     {
         [SerializeField] private GameObject settings;
 
-        public void SetMusicVolume(float volume)
+        [SerializeField] private TextMeshProUGUI musicText;
+        [SerializeField] private TextMeshProUGUI effectsText;
+
+        [SerializeField] private Slider musicSlider;
+        [SerializeField] private Slider effectsSlider;
+
+        public void HandleMusicVolumeChanged()
         {
+            var volume = musicSlider.value;
+            musicText.SetText($"Music Volume: {volume.ToString("0.00", CultureInfo.InvariantCulture)}");
             PlayerPrefs.SetFloat("MusicVolume", volume);
         }
 
-        public void SetSoundEffectsVolume(float volume)
+        public void HandleSoundEffectsVolumeChanged()
         {
+            var volume = effectsSlider.value;
+            effectsText.SetText($"Sound Effects Volume: {volume.ToString("0.00", CultureInfo.InvariantCulture)}");
             PlayerPrefs.SetFloat("EffectsVolume", volume);
         }
 
         public void EnableSettingsMenu()
         {
+            musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
+            effectsSlider.value = PlayerPrefs.GetFloat("EffectsVolume", 1f);
+
+            HandleMusicVolumeChanged();
+            HandleSoundEffectsVolumeChanged();
+
             settings.SetActive(true);
         }
 
